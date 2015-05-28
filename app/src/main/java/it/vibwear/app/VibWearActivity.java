cfbb5388f1019253b955ebe83ecc8a2fd11c4c70
biospank <fabio.petrucci@gmail.com>
2015-05-28 17:47:15 +0100

    Fix notification builder

--- app/src/main/java/it/vibwear/app/VibWearActivity.java
+++ app/src/main/java/it/vibwear/app/VibWearActivity.java
@@ -31,7 +31,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class VibWearActivity extends ModuleActivity implements OnLocationChangeListener, SettingsDetailFragment.OnSettingsChangeListener, AlarmListner {
-	private static final String VERSION = "1.3.8";
+	private static final String VERSION = "1.3.9";
 	private static final long SIGNAL_START_DELAY = 10000;
 	private static final long SIGNAL_SCHEDULE_TIME = 5000;
 	private static final long BATTERY_START_DELAY = 60000;
@@ -248,10 +248,10 @@ public class VibWearActivity extends ModuleActivity implements OnLocationChangeL
 			ft.add(R.id.servicesLayout, servicesFrag, "servicesFragment");
 			ft.commit();
 
-			mBuilder = new Notification.Builder(this);
 		}
 		
 		powerMgr = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		mBuilder = new Notification.Builder(this);
 
 		intentFilter = new IntentFilter();
 		intentFilter.addAction(ServicesFragment.CALL_VIB_ACTION);
