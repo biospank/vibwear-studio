47faf12b9bad9d88324764a59d45f1b7c2f7edbc
biospank <fabio.petrucci@gmail.com>
2016-03-11 20:59:54 +0000

    Bump to version 1.7.2

--- app/src/main/java/it/vibwear/app/VibWearActivity.java
+++ app/src/main/java/it/vibwear/app/VibWearActivity.java
@@ -48,7 +48,7 @@ public class VibWearActivity extends ModuleActivity implements
         ScannerFragment.OnDeviceSelectedListener, OnLocationChangeListener,
         SettingsDetailFragment.OnSettingsChangeListener, AlarmListner {
 
-	private static final String VERSION = "1.7.1";
+	private static final String VERSION = "1.7.2";
 	private static final long SIGNAL_START_DELAY = 10000;
 	private static final long SIGNAL_SCHEDULE_TIME = 15000;
 	private static final long BATTERY_START_DELAY = 60000;
